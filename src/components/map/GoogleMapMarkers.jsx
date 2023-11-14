11c14c0e36f8c2fc739b32a363c21b9b9fc7b4b4
Piotr Ostrowski <piotro@student.chalmers.se>
2023-11-14 16:59:41 +0000

    Resolve "Create a navbar to display login button and status"

--- src/components/map/GoogleMapMarkers.jsx
+++ src/components/map/GoogleMapMarkers.jsx
@@ -2,30 +2,30 @@
 import { useEffect } from 'react'
 
 function GoogleMapMarkers(props) {
-    useEffect(() => {
-        if (!props.map) {
-            return
+  useEffect(() => {
+    if (!props.map) {
+      return
+    }
+
+    const newMarkers = props.markers.map((data, index) => {
+      return new google.maps.Marker({
+        map: props.map,
+        title: data.title,
+        position: {
+          lat: data.position.lat,
+          lng: data.position.lng,
         }
+      })
+    })
 
-        const newMarkers = props.markers.map((data, index) => {
-            return new google.maps.Marker({
-                map: props.map,
-                title: data.title,
-                position: {
-                    lat: data.position.lat,
-                    lng: data.position.lng,
-                }
-            })
-        })
+    const latLngBounds = new google.maps.LatLngBounds()
+    newMarkers.forEach(marker => latLngBounds.extend(marker.getPosition()))
+    props.map.fitBounds(latLngBounds)
 
-        const latLngBounds = new google.maps.LatLngBounds()
-        newMarkers.forEach(marker => latLngBounds.extend(marker.getPosition()))
-        props.map.fitBounds(latLngBounds)
+    return () => newMarkers.forEach(marker => marker.setMap(null))
+  }, [props.map, props.markers])
 
-        return () => newMarkers.forEach(marker => marker.setMap(null))
-    }, [props.map, props.markers])
-
-    return null
+  return null
 }
 
 export default GoogleMapMarkers
