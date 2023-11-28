9e66e744ed7be327b797b9a83b8f4fc8cb293bca
Linh Pham <guslinhph@student.gu.se>
2023-11-28 23:39:18 +0100

    #1 init a project

---
+++ Models/patientSchema.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const patientSchema = mongoose.Schema(
+    {
+        Personnummer: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        Firstname: {
+            type: String,
+            required: true
+        },
+        Lastname: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        }
+    });
+
+exports.Patient = mongoose.model('patient', patientSchema)
