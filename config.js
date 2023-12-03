4ae317ff014c3c5af894fab3e25cd264f4917822
raghavexe <113340539+raghavexe@users.noreply.github.com>
2023-12-03 12:51:21 +0100

    resolve issues

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 export const PORT = 8080;
 
 // topics for mqtt broker
-export const patient_publish_create = 'patient/pub/create'
+export const patient_publish_create = 'patient/create'
+export const patient_subscribe_create = 'patient/query'
--- routes/patientRoute.js
+++ routes/patientRoute.js
@@ -1,5 +1,5 @@
-import {initializeMqttUsingEnvVariables,publish} from 'mqtt-service'
-import {patient_publish_create} from '../config.js'
+import {initializeMqttUsingEnvVariables,publishAwaitingResponse, subscribe} from 'mqtt-service'
+import {patient_publish_create, patient_subscribe_create} from '../config.js'
 import express from 'express'
 const router = express.Router()
 
@@ -13,29 +13,32 @@ try {
         !req.body.Personnummer||
         !req.body.Firstname||
         !req.body.Lastname||
-        !req.body.age||
         !req.body.password||
         !req.body.email
     ){
-        res.status(400).json('Credentials are missing')
+        res.status(400).json({msg:'Credentials are missing'})
     }
         console.log("creating new patient")
         const newPatient = {
             Personnummer: req.body.Personnummer,
             Firstname: req.body.Firstname,
             Lastname: req.body.Lastname,
-            age: req.body.age,
             password: req.body.password,
             email: req.body.email
         }
         
-        publish(patient_publish_create,JSON.stringify(newPatient))
-        res.status(201).json('Patient created and published successfully')
-    
-    
-    
+        publishAwaitingResponse(patient_publish_create,JSON.stringify({Personnummer:Personnummer}), (patient_subscribe_create,payload,packet) =>{
+
+            if(JSON.stringify(payload) === 'could not find'){
+                publish(patient_publish_create,JSON.stringify(newPatient))
+                res.status(201).json({msg:'Patient created and published successfully'})
+            } else{
+                res.status(409).json({msg:'Account already exists'})
+
+            }
+        })
 } catch (error) {
-    return res.status(500).json('patient creation unsuccessful')
+    return res.status(500).json({msg:'patient creation unsuccessful'})
 }
 })
 
