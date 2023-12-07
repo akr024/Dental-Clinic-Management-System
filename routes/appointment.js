afd00237db2415edd263c9cb65740ceeae2bbf37
raghavexe <113340539+raghavexe@users.noreply.github.com>
2023-12-07 13:44:21 +0100

    add ids

--- routes/appointment.js
+++ routes/appointment.js
@@ -6,11 +6,19 @@ const router = express.Router();
 router.post('/', async (req,res) => {
     try {
         console.log("entering if loop")
-        if(!req.body.dateTime){
+        if( 
+            //!req.body.dateTime||
+            !req.body.clinicId||
+            !req.body.patientId||
+            !req.body.dentistId
+            ){
             res.status(400).json({msg:'Date and time is missing'})
-        }
+        } else{
             console.log("creating new appointment")
             const newAppointment = {
+                clinicId: req.body.clinicId,
+                dentistId: req.body.dentistId,
+                patientId: req.body.patientId,
                 dateTime: req.body.dateTime
             }
             publishAwaitingResponse(appointment_publish_create,JSON.stringify(newAppointment),(topic,payload,packet)=>{
@@ -22,6 +30,9 @@ router.post('/', async (req,res) => {
                     res.status(400).json(response.msg)
                 }      
             })
+
+        }
+            
             
     } catch (error) {
         return res.status(500).json({msg:'appointment creation unsuccessful'})
