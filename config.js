0817655cf3dcf30f81a4358688bca063e95414a3
raghavexe <113340539+raghavexe@users.noreply.github.com>
2023-12-07 12:42:01 +0100

    POST method functionality+ older deletions

---
+++ config.js
@@ -0,0 +1,7 @@
+export const PORT = 3000;
+
+export const appointment_publish_create = 'appointment/create'
+export const appointment_subscribe_create = 'appointment/query'
+
+
+
---
+++ routes/appointment.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import {publishAwaitingResponse} from 'mqtt-service'
+import { appointment_publish_create } from '../config.js';
+const router = express.Router();
+
+router.post('/', async (req,res) => {
+    try {
+        console.log("entering if loop")
+        if(!req.body.dateTime){
+            res.status(400).json({msg:'Date and time is missing'})
+        }
+            console.log("creating new appointment")
+            const newAppointment = {
+                dateTime: req.body.dateTime
+            }
+            publishAwaitingResponse(appointment_publish_create,JSON.stringify(newAppointment),(topic,payload,packet)=>{
+    
+                const response = JSON.parse(payload.toString())
+                if(response.success){
+                    res.status(201).json(response.newAppointment)
+                } else{
+                    res.status(400).json(response.msg)
+                }      
+            })
+            
+    } catch (error) {
+        return res.status(500).json({msg:'appointment creation unsuccessful'})
+    }       
+})
+
+export default router;
