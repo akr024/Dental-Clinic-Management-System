85c2e2a1057b36419e3e140b1076121816042a8b
mais <joubsen@net.chalmers.se>
2023-12-09 18:31:25 +0100

    #8: deleted extra dependencies

--- routes/routes.js
+++ routes/routes.js
@@ -30,7 +30,7 @@ router.post(
               if (error) return next(error);
 
               const body = { _id: user._id, email: user.email };
-              const token = jwt.sign({ user: body }, 'TOP_SECRET');
+              const token = jwt.sign({ user: body }, 'TOP_SECRET', { expiresIn: '1h' }); 
 
               return res.json({ token });
             }
