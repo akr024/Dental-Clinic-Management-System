a5f3661b4f3a7118a83e9c1fe9b657fd5a651b64
mais <joubsen@net.chalmers.se>
2024-01-07 15:42:06 +0100

    #10:delete unused file clinicSchema

--- utils/authConfig.js
+++ utils/authConfig.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const localStrategy = require('passport-local').Strategy;
 const PatientModel = require('../models/Patient')
 const DentistModel = require('../models/dentistSchema')
-require('../models/ClinicModel')
+require('../models/clinicSchema')
 passport.use(
   'loginPatient',
   new localStrategy(
@@ -13,8 +13,6 @@ passport.use(
     async (personnummer, password, done) => {
       try {
         let user = await PatientModel.findOne({ Personnummer: personnummer });
-        let userList = await PatientModel.find();
-        console.log(userList)
         if (!user) {
           return done(null, false, { message: 'User not found' });
         }
