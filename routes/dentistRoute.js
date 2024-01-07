63fb44cc0ea29417eaa6bba04af409f7d3a1bd49
raghavexe <113340539+raghavexe@users.noreply.github.com>
2024-01-07 17:51:58 +0100

    change post method

--- routes/dentistRoute.js
+++ routes/dentistRoute.js
@@ -3,45 +3,46 @@ import {publishAwaitingResponse} from 'mqtt-service'
 import { TOPIC_DENTIST_CREATE, TOPIC_DENTIST_MODIFY, TOPIC_DENTIST_QUERY } from '../config.js'
 const router = express.Router();
 
-router.post('/',async(req,res)=>{
+router.post('/', async (req, res) => {
     try {
-    if(
-        !req.body.personnummer||
-        !req.body.firstName||
-        !req.body.lastName||
-        !req.body.password||
-        !req.body.email
-    ){
-        res.status(400).json({msg:'Credentials missing'})
-    } else{
+        if (
+            !req.body.personnummer ||
+            !req.body.firstName ||
+            !req.body.lastName ||
+            !req.body.password ||
+            !req.body.email
+        ) {
+            res.status(400).json({ msg: 'Credentials are missing' });
+            return;
+        }
+
         const newDentist = {
             personnummer: req.body.personnummer,
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             password: req.body.password,
-            email: req.body.email
-        }
-        publishAwaitingResponse(TOPIC_DENTIST_CREATE,JSON.stringify(newDentist),(topic,payload,packet)=>{
-            console.log('\x1b[95m%s\x1b[0m', '[Dentist JSON object sent from dentist API..]');
-            const response = JSON.parse(payload.toString())
-            if(response.success){
-               res.status(201).json(response.newDentist)
-            } else{
-                res.status(400).json({msg: response.msg})
-            } 
-        })
-    }
+            email: req.body.email,
+        };
+        console.log(newDentist)
+        publishAwaitingResponse(TOPIC_DENTIST_CREATE, JSON.stringify(newDentist), (topic, payload, packet) => {
+            const response = JSON.parse(payload.toString());
+            if (response.success) {
+                res.status(201).json(response.newDentist);
+            } else {
+                res.status(400).json({ msg: response.msg });
+            }
+        });
     } catch (error) {
-        res.status(400).json({msg: 'dentist creation unsuccessful'})
+        res.status(500).json({ msg: 'Dentist creation unsuccessful' });
     }
+});
 
-})
 
 router.put('/',async(req,res)=>{
     const dentistUpdates = req.body
     try{
         let dentist;
-        publishAwaitingResponse(TOPIC_DENTIST_QUERY,JSON.stringify(dentistUpdates.personnummer),(topic,payload,packet)=>{
+        publishAwaitingResponse(TOPIC_DENTIST_QUERY,JSON.stringify(dentistUpdates._id),(topic,payload,packet)=>{
             const response = JSON.parse(payload.toString());
             if(response.success){
                  dentist = response
