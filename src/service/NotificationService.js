1089baaec3c8f844f07f6a1e3e82fb38652559f1
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2024-01-11 20:16:32 +0100

    minor changes to the name of the attributes in the database for consistency

--- src/service/NotificationService.js
+++ src/service/NotificationService.js
@@ -32,8 +32,8 @@ async function createNotificationBook(inputData) {
         const newNotificationDoctor = await new Notification({
             title: `Appointment booked on ${dateTime}`,
             time: new Date(),
-            desc: `Appointment for Dentist (${inputData.dentistId}) has been booked at clinic (${inputData.clinicId}) by Patient (${inputData.patient})`,
-            to: [`${await getEmailDentist(inputData.dentistId)}`, `${await getEmailPatient(inputData.patient)}`]
+            desc: `Appointment for Dentist (${inputData.dentistId}) has been booked at clinic (${inputData.clinicId}) by Patient (${inputData.patientId})`,
+            to: [`${await getEmailDentist(inputData.dentistId)}`, `${await getEmailPatient(inputData.patientId)}`]
         }).save()
 
         sendEmail(newNotificationDoctor);
@@ -54,7 +54,7 @@ async function createNotificationCancel(inputData) {
             title: `Appointment (${inputData.appointmentId}) on ${dateTime} cancelled`,
             time: new Date(),
             desc: `Appointment with ID: ${inputData.appointmentId} on ${dateTime} has been cancelled at clinic (${inputData.clinicId}) by Dentist (${inputData.dentistId})`,
-            to: [`${await getEmailDentist(inputData.dentistId)}`, `${await getEmailPatient(inputData.patient)}`]
+            to: [`${await getEmailDentist(inputData.dentistId)}`, `${await getEmailPatient(inputData.patientId)}`]
         }).save()
 
         sendEmail(newNotificationPatient);
